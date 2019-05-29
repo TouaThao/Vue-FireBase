9e4bb0886fac0f02a069dfaf5f8b7102ae7085da
Toua Thao <touathao09@gmail.com>
2019-05-29 13:25:40 -0500

    axios

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<Blog/>
+</template>
+
+<script>
+import Blog from './Blog'
+export default {
+  name: 'app',
+  components:{
+    Blog
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/Blog.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      <h2>Blog</h2>
+      <div v-for="post in posts" :key="post.id">
+          <h3>
+              {{post.title}}
+          </h3>
+          <p>
+              {{post.body}}
+          </p>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name:'Blog',
+    data (){
+        return{
+            posts:[]
+        }
+    },
+    methods:{
+
+    },
+    created(){
+        axios.get('https://jsonplaceholder.typicode.com/posts/')
+        .then(response=>{
+            this.posts = response.data
+        }).catch(err=>{
+            console.log('error',err)
+        })
+    }
+};
+</script>
+
+<style>
+
+</style>
